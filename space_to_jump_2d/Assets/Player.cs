cf7a398a64784d78c1a32692d85bf16008af249b
Jacob White <71794641+whiteja2@users.noreply.github.com>
2023-03-31 02:12:13 -0700

    Add files via upload

---
+++ space_to_jump_2d/Assets/Player.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public bool grounded = true;
+    private Rigidbody2D rb2d;
+    public float jumpPower;
+    private BoxCollider2D boxCollider2D;
+    [SerializeField] private LayerMask platformsLayerMask;
+    public float speed = 10f;
+
+    void Start()
+    {
+        rb2d = rb2d = GetComponent<Rigidbody2D> ();
+        boxCollider2D = transform.GetComponent<BoxCollider2D>();
+    }
+
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Space) && IsGrounded()) {
+            rb2d.velocity = Vector2.up * jumpPower;
+        }
+
+        Vector3 pos = transform.position;
+        if (Input.GetKey("d"))
+        {
+            pos.x += speed * Time.deltaTime;
+        }
+        if (Input.GetKey("a"))
+        {
+            pos.x -= speed * Time.deltaTime;
+        }
+        transform.position = pos;
+    }
+
+    private bool IsGrounded() {
+        RaycastHit2D raycastHit2D = Physics2D.BoxCast(boxCollider2D.bounds.center, boxCollider2D.bounds.size, 0f, Vector2.down, 0.1f, platformsLayerMask);
+        return raycastHit2D.collider != null;
+    }
+}
